76ff0f1be55f5a6862aed9ccd7b8ab07d155f281
Louis De Macedo <louis.de.macedo.ldm@gmail.com>
2022-05-11 01:32:36 +0200

    [update] changes id statements using function pointer look up table

--- ed.py
+++ ed.py
@@ -11,6 +11,90 @@ NB_PRT_MODE = 4
 PRT_MODE = 5
 EX_MODE = 6
 
+
+def quit_cmd(_buffer: list[str], _line: str, cursor: int)\
+-> tuple[int, int]:
+    return cursor, EX_MODE
+
+def change_cmd(_buffer: list[str], _line: str, cursor: int)\
+-> tuple[int, int]:
+    return cursor, CHG_MODE
+
+def append_cmd(_buffer: list[str], _line: str, cursor: int)\
+-> tuple[int, int]:
+    return cursor, APP_MODE
+
+def insert_cmd(_buffer: list[str], _line: str, cursor: int)\
+-> tuple[int, int]:
+    return cursor, INS_MODE
+
+def delete_cmd(buffer: list[str], _line: str, cursor: int)\
+-> tuple[int, int]:
+    buffer.pop(cursor)
+    print(buffer[cursor], end='')
+    return cursor, CMD_MODE
+
+def print_cmd(buffer: list[str], _line: str, cursor: int)\
+-> tuple[int, int]:
+    print(buffer[cursor],end='')
+    return cursor, CMD_MODE
+
+def printall_cmd(buffer: list[str], _line: str, cursor: int)\
+-> tuple[int, int]:
+    for ln in buffer:
+        print(ln, end='')
+    return cursor, CMD_MODE
+
+def printnb_cmd(buffer: list[str], _line: str, cursor: int)\
+-> tuple[int, int]:
+    print("%d\t| %s" % (cursor + 1, buffer[cursor]), end='')
+    return cursor, CMD_MODE
+
+def printnball_cmd(buffer: list[str], _line: str, cursor: int)\
+-> tuple[int, int]:
+    for i in range(len(buffer)):
+        print("%d\t| %s" % (i + 1, buffer[i]), end='')
+    return cursor, CMD_MODE
+
+def write_cmd(buffer: list[str], line: str, cursor: int)\
+-> tuple[int, int]:
+    with open(line[2:], 'w') as file:
+        file.writelines(buffer)
+    return cursor, CMD_MODE
+
+def edit_cmd(buffer: list[str], line: str, cursor: int)\
+-> tuple[int, int]:
+    with open(line[2:], 'r') as file:
+        buffer.clear()
+        buffer.extend(file.readlines())
+    return cursor, CMD_MODE
+
+def lastline_cmd(buffer: list[str], _line: str, _cursor: int)\
+-> tuple[int, int]:
+    return len(buffer) - 1, CMD_MODE
+
+def error_cmd(_buffer: list[str], _line: str, cursor: int)\
+-> tuple[int, int]:
+    print("?")
+    return cursor, CMD_MODE
+
+
+OP_LOT = {
+    'q': quit_cmd,
+    'Q': quit_cmd,
+    'c': change_cmd,
+    'a': append_cmd,
+    'i': insert_cmd,
+    'd': delete_cmd,
+    'p': print_cmd,
+    'P': printall_cmd,
+    'n': printnb_cmd,
+    'N': printnball_cmd,
+    'w': write_cmd,
+    'e': edit_cmd,
+    '$': lastline_cmd
+}
+
 def get_command_input(buffer: list[str], cursor: int, _: int)\
 -> tuple[int, int]:
     line: str = input()
@@ -18,50 +102,12 @@ def get_command_input(buffer: list[str], cursor: int, _: int)\
         print("?")
         return cursor, CMD_MODE
     try:
-        if line[0] == 'Q' or line[0] == 'q':
-            return cursor, EX_MODE
-        if line[0] == 'c':
-            return cursor, CHG_MODE
-        if line[0] == 'a':
-            return cursor, APP_MODE
-        if line[0] == 'i':
-            return cursor, INS_MODE
-        if line[0] == 'd':
-            buffer.pop(cursor)
-            print(buffer[cursor], end='')
-            return cursor, CMD_MODE
-        if line[0] == 'p':
-            print(buffer[cursor],end='')
-            return cursor, CMD_MODE
-        if line[0] == 'P':
-            for line in buffer:
-                print(line, end='')
-            return cursor, CMD_MODE
-        if line[0] == 'n':
-            print("%d\t| %s" % (cursor + 1, buffer[cursor]), end='')
-            return cursor, CMD_MODE
-        if line[0] == 'N':
-            for i in range(len(buffer)):
-                print("%d\t| %s" % (i + 1, buffer[i]), end='')
-            return cursor, CMD_MODE
-        if line[0] == 'w':
-            with open(line[2:], 'w') as file:
-                file.writelines(buffer)
-            return cursor, CMD_MODE
-        if line[0] == 'e':
-            with open(line[2:], 'r') as file:
-                buffer.clear()
-                buffer.extend(file.readlines())
-            return cursor, CMD_MODE
-        if line[0] == '$':
-            return len(buffer) - 1, CMD_MODE
         if type(int(line)) is int:
             print(buffer[int(line) - 1], end='')
             return int(line) - 1, CMD_MODE
     except (ValueError, IndexError):
         pass
-    print("?")
-    return cursor, CMD_MODE
+    return OP_LOT.get(line[0], error_cmd)(buffer, line, cursor)
 
 def get_text_input(buffer: list[str], cursor: int, mode: int)\
 -> tuple[int, int]:
